8477342ec7d37fd52ff8be5e6d9eb4043005b395
Полина Боровкова <kawaiiipolly@gmail.com>
2021-12-14 22:51:52 +0300

    /

--- InternetShop/Program.cs
+++ InternetShop/Program.cs
@@ -18,7 +18,6 @@ namespace InternetShop
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new OtherForm.Login());
-            //привет полина как дела у тебя
         }
     }
 }
