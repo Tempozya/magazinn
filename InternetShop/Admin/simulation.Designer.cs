b0e0f60a185e137ca063c810fdef702a9abfe7c0
Артём Семёнов <timo.semenow@yandex.ru>
2021-12-21 22:10:59 +0300

    Симуляция

--- InternetShop/Admin/simulation.Designer.cs
+++ InternetShop/Admin/simulation.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.labeldate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(201, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кол-во пользователей";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(383, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelCount.Location = new System.Drawing.Point(376, 88);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(19, 21);
+            this.labelCount.TabIndex = 3;
+            this.labelCount.Text = "К";
+            // 
+            // labeldate
+            // 
+            this.labeldate.AutoSize = true;
+            this.labeldate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labeldate.Location = new System.Drawing.Point(376, 57);
+            this.labeldate.Name = "labeldate";
+            this.labeldate.Size = new System.Drawing.Size(19, 21);
+            this.labeldate.TabIndex = 4;
+            this.labeldate.Text = "К";
+            // 
+            // simulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labeldate);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "simulation";
             this.Text = "simulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label labeldate;
     }
 }
--- InternetShop/Admin/simulation.cs
+++ InternetShop/Admin/simulation.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using InternetShop.Script;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,97 @@ namespace InternetShop.Admin
 {
     public partial class simulation : Form
     {
+        sqlFunc sql = new sqlFunc();
         public simulation()
         {
             InitializeComponent();
+        }
+
+
+
+        private bool working = false;
+        private int countUser = 0;
+        private DateTime date = new DateTime(2021, 12, 1);
+
+
+        public void SimpleGenerateUser()
+        {
+            string id, login, password, name, surname, phone;
+
+            string[] Arrlogin = { "KOD", "ральф", "ХУЛИGАН", "Dreamer", "The_Red_Hunter", "Qwerty", "Orissi", "Waldo" };
+            string[] Arrpassword = { "Al1aHRXT9i3psaT", "qGQZqX7HGN6GUYk", "QAxd40eviRSHnOZ", "18fG9bJkAIGQcTs", "XjUo4NmTjdDGNPz", "3kMkHpZG659hF5e" };
+            string[] Arrname = { "Эдуард", "Устин", "Цицерон", "Зенон", "Марат", "Эрик", "Юлиан", "Йоханес", "Харитон", "Станислав", "Йонас", "Клаус" };
+            string[] Arrsurname = { "Коновалов", "Иванов", "Цветков", "Назаров", "Ермаков", "Пестов", "Архипов", "Горбачёв", "Красинец", "Гуляев" };
+            string[] Arrphone = { "111765958", "753069090", "277869249", "277869249", "857907618", "549071467", "939915908", "588554078", "782785473", "609017116" };
+
+            Random random = new Random();
+
+            while (working)
+            {
+                id = random.Next(1000, 99999).ToString();
+                login = Arrlogin[random.Next(0, Arrlogin.Length - 1)];
+                password = Arrpassword[random.Next(0, Arrpassword.Length - 1)];
+                name = Arrname[random.Next(0, Arrname.Length - 1)];
+                surname = Arrsurname[random.Next(0, Arrsurname.Length - 1)];
+                phone = Arrphone[random.Next(0, Arrphone.Length - 1)];
+
+
+                countUser++;
+
+
+
+                sql.RegUser(id, login, password, name,phone);
+
+                Thread.Sleep(500);
+
+            }
+
+
+
+        }
+
+        private void SimLoop()
+        {
+            while (working)
+            {
+                if (date.DayOfWeek == DayOfWeek.Monday)
+                {
+
+                }
+                labelCount.Invoke(new Action(() => labelCount.Text = countUser.ToString()));
+
+                labeldate.Invoke(new Action(() => labeldate.Text = date.ToString("d")));
+
+                date = date.AddDays(1);
+                Thread.Sleep(1000);
+
+            }
+        }
+
+
+
+
+        private void Start()
+        {
+            working = true;
+            Task.Run(() => SimLoop());
+            Task.Run(() => SimpleGenerateUser());
+        }
+
+        private void Stop()
+        {
+            working = false;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stop();
         }
     }
 }
